692f109d799807cc53efdaf0fb05bd825ca8ccf5
Leo Pan <kinglionleo.8@gmail.com>
2024-01-20 14:52:49 -0500

    Added room creation

--- server/models/User.js
+++ server/models/User.js
@@ -1,7 +1,15 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-    
+    roomCode: {
+        type: String,
+        required: true,
+    },
+    username: {
+        type: String,
+        required: true,
+        default: "Anonymous"
+    }
 });
 
 module.exports = mongoose.model('User', UserSchema);
--- server/routes/rooms.js
+++ server/routes/rooms.js
@@ -1,11 +1,28 @@
 const express = require('express');
+const Room = require('../models/Room');
 
 const router = express.Router();
 
 // Create Exercise
 router.post('/create', async (req, res) => {
   try {
+    let code = "";
+    for(let i=0; i<4; i++) {
+      code += String.fromCharCode(65 + Math.floor(Math.random() * 26));
+    }
     
+    let room = new Room({
+      code: code
+    });
+    
+    await room.save();
+
+    if (!room) {
+      return res.status(401).json('Duplicate code accidentally generated, please try again!');
+    }
+
+    console.log(`Room created with code ${code}`)
+    return res.status(200).json(room);
 
   } catch (error) {
     res.status(500).json({ message: 'Server error' });

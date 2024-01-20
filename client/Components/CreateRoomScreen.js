3f8c205017b1ab72ceea8d3393b74780b7e2a6e0
Chrislee3x7 <myemail@example.com>
2024-01-20 15:13:43 -0500

    added navigation

--- client/Components/CreateRoomScreen.js
+++ client/Components/CreateRoomScreen.js
@@ -9,18 +9,19 @@ const CreateRoomScreen = ({ navigation }) => {
   const insets = useSafeAreaInsets();
 
   return (
-    <View className="bg-blue-400 justify-center grow" style={{paddingTop: insets.top, paddingBottom: insets.bottom}}>
-      <View className="">
-        <Text className="mt-6 mb-4 mx-4 text-center" variant="headlineLarge">New Room</Text>
-        <View className="mb-4 ml-4 mr-4">
-          <TextInput
-            autoCapitalize="none"
-            blurOnSubmit
-            inputMode="text"
-            mode='outlined'
-            label='Username'
-          />
-        </View>
+    <View className="px-8 bg-blue-400 justify-center grow" style={{paddingTop: insets.top, paddingBottom: insets.bottom}}>
+      <Text className="text-center" variant="titleSmall">Room Code:</Text>
+      <Text className="mb-4 mx-4 text-center" variant="headlineLarge">RDQJJ</Text>
+      <View className="mb-4 ml-4 mr-4">
+        <TextInput
+          autoCapitalize="none"
+          blurOnSubmit
+          inputMode="text"
+          mode='outlined'
+          label='Username'
+        />
+      </View>
+      <View className="justify-center">
         <Button className="my-4 mx-2" mode='contained' 
           onPress={() => navigation.navigate('Home')}>Start Room</Button>
       </View>
--- client/Components/HomeScreen.js
+++ client/Components/HomeScreen.js
@@ -4,7 +4,7 @@ import { View } from 'react-native';
 import { Button, Text, TextInput } from 'react-native-paper';
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 
-const HomeScreen = () => {
+const HomeScreen = ({ navigation }) => {
   const insets = useSafeAreaInsets();
 
   return (
@@ -14,7 +14,7 @@ const HomeScreen = () => {
         </View>
         <View className="flex grow pl-8 pr-8">
           <Button className="mx-2 my-4" mode='contained' 
-            onPress={() => onLoginPress()}>Create Room</Button>
+            onPress={() => navigation.navigate("CreateRoom")}>Create Room</Button>
 
           <Button className="mx-2 my-4 pl-8 pr-8" mode='contained-tonal' 
             onPress={() => onLoginPress()}>Join Room</Button>

5da10a8108818add72671292e98982940bdc5479
andrewtu517 <andrewtu517@gmail.com>
2024-01-20 16:06:04 -0500

    Go Back Join Room Screen

--- client/Components/JoinRoomScreen.js
+++ client/Components/JoinRoomScreen.js
@@ -1,35 +1,51 @@
 import React, { useState } from 'react';
-import { View } from 'react-native';
-import { Button, Text, TextInput } from 'react-native-paper';
+import { View, TouchableOpacity } from 'react-native';
+import { Button, Text, TextInput, Icon } from 'react-native-paper';
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
+
 
 const HomeScreen = ({ navigation }) => {
   const insets = useSafeAreaInsets();
 
   return (
-    <View className="grow bg-blue-400" style={{paddingTop: insets.top, paddingBottom: insets.bottom}}>
-        <View className="flex h-1/2">
+    <View className="grow bg-blue-400" style={{ paddingTop: insets.top, paddingBottom: insets.bottom }}>
+      <View className="flex h-1/2">
+        <TouchableOpacity className="flex-row ml-4 items-center" onPress={() => navigation.goBack()}>
+          <Icon
+            source="chevron-left"
+            color= "black"
+            size={35}
+          />
+          <Text>Go Back</Text>
 
+        </TouchableOpacity>
+        {/* <Button
+          icon="chevron-left"
+          iconColor="black"
+          size={50}
+          onPress={() => console.log('Pressed')}
+        >Back</Button> */}
+
+      </View>
+      <View className="flex grow pl-8 pr-8">
+        <View className="mb-4 ml-4 mr-4">
+          <TextInput
+            autoCapitalize="none"
+            blurOnSubmit
+            inputMode="text"
+            mode='outlined'
+            label='Room Code'
+          />
         </View>
-          <View className="flex grow pl-8 pr-8">
-          <View className="mb-4 ml-4 mr-4">
-            <TextInput
-              autoCapitalize="none"
-              blurOnSubmit
-              inputMode="text"
-              mode='outlined'
-              label='Room Code'
-            />
-          </View>
 
-          <Button 
-            className="mx-2 my-4 pl-8 pr-8" 
-            mode='contained-tonal' 
-            // labelStyle={{}}
-            onPress={() => navigation.navigate("JoinRoom")}
-          >Join Room
-          </Button>
-        </View> 
+        <Button
+          className="mx-2 my-4 pl-8 pr-8"
+          mode='contained-tonal'
+          // labelStyle={{}}
+          onPress={() => navigation.navigate("JoinRoom")}
+        >Join Room
+        </Button>
+      </View>
     </View>
   )
 }
